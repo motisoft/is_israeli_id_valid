d640a8a33184f42de66a7636c7e6c4290a319d81
Moti <motish48@gmail.com>
2020-05-24 19:33:44 +0300

    initial commit

---
+++ mod.ts
@@ -0,0 +1,21 @@
+
+/** Israeli government issued ID number `check digit validation only` module. */
+export function isIsraeliIdValid(id: string): boolean {
+  id = id.trim();
+  if (id.length < 2 || id.length > 9) return false;
+
+  let num = Number(id);
+  if (num == NaN || num < 18) return false;
+
+  while (id.length < 9) id = "0" + id;
+
+  let counter = 0,
+    rawVal,
+    actualVal;
+  for (let i = 0; i < id.length; i++) {
+    rawVal = Number(id[i]) * ((i % 2) + 1);
+    actualVal = rawVal > 9 ? rawVal - 9 : rawVal;
+    counter += actualVal;
+  }
+  return counter % 10 === 0;
+}
